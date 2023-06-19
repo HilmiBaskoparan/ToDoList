45d3a06cc3198dd18e7c37acd29230b9137f13d2
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-06-19 23:11:38 +0300

    Create Custom Exceptions. Close #2

---
+++ src/main/java/com/hilmibaskoparan/exception/MyException.java
@@ -0,0 +1,9 @@
+package com.hilmibaskoparan.exception;
+
+// My Special Exception
+public class MyException extends RuntimeException {
+
+    public MyException(String message) {
+        super(message);
+    }
+}
