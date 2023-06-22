016c1112e2892457303b8550bb8d66043b09a5fe
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-06-22 15:38:18 +0300

    Adding PersistLinks

---
+++ src/main/java/com/hilmibaskoparan/util/PersistLink.java
@@ -0,0 +1,24 @@
+package com.hilmibaskoparan.util;
+
+public class PersistLink {
+
+    /*
+        Github URL
+        https://github.com/HilmiBaskoparan
+        https://github.com/HilmiBaskoparan/ToDoList
+
+        Backend URL
+        http://localhost:4040/
+
+        H2DB H2-Console
+        http://localhost:4040/h2-console
+
+        Swagger URL
+        http://localhost:4040/swagger-ui/index.html
+    */
+
+
+
+
+
+}
