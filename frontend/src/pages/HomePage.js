a941a6b83da95fd6bd447ed4650805165ac6db31
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-06-26 22:29:22 +0300

    Web Page Created (Html and Css)

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -37,6 +37,43 @@ function HomePage() {
                     <button className="btn btn-primary btn-md col m-3">InCompleted</button>
                 </div>
             </div>
+            
+            <div className="row mx-1 px-5 pb-3 w-80">
+                <div className="col mx-auto">
+                    <div className="row px-3 align-items-center todo-item rounded border">
+                        <div className="col-auto m-1 p-0 d-flex align-items-center">
+                            <h2 className="m-0 p-0">
+                                <i className="fa fa-square-o text-primary btn m-0 p-0 d-none" data-toogle="tooltip" data-placement="bottom" title="Mark as Complete"></i>
+                                <input type="checkbox" className="form-control-lg" />
+                            </h2>
+                        </div>
+                        <div className="col px-1 m-1 d-flex align-items-center">
+                            <input type="text" className="form-control border-0 edit-todo-input bg-transparent rounded px-3 text-x1" title="Todo Item #1" readOnly disabled/>
+                        </div>
+
+                        <div className="col-auto m-1 p-0 px-3 d-none"></div>
+
+                        <div className="col-auto m-1 p-0 todo-actions">
+                            <div className="col d-flex align-items-center justify-content-end">
+                                <h5 className="m-0 p-0 px-2">
+                                    <button type="button" className="btn btn-primary">Edit</button>
+                                </h5>
+                                <h5 className="m-0 p-0 px-2">
+                                    <button type="button" className="btn btn-danger">Delete</button>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-4 mb-4 col d-flex align-items-center justify-content-center">
+                <h5 className="m-0 p-0 px-2">
+                    <button type="button" className="btn btn-danger">Delete Done Tasks</button>
+                </h5>
+                <h5 className="m-0 p-0 px-2">
+                    <button type="button" className="btn btn-danger">Delete All Tasks</button>
+                </h5>
+            </div>
         </div>
     );
 
