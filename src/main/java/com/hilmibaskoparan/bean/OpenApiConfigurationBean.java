ef5e39626fa20b4c82313efd6d5fa986ec2219a9
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-06-27 20:31:53 +0300

    OpenApiConfigurationBean is added

---
+++ src/main/java/com/hilmibaskoparan/bean/OpenApiConfigurationBean.java
@@ -0,0 +1,28 @@
+package com.hilmibaskoparan.bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfigurationBean {
+    @Bean
+    public OpenAPI openAPIMethod() {
+        return new OpenAPI().info(new Info()
+                .title("Todo List")
+                .description("Todo List Project with Spring Boot using React.js")
+                .version("V1.0")
+                .contact(new Contact()
+                        .name("Hilmi Başkoparan")
+                        .url("https://github.com/HilmiBaskoparan")
+                        .email("h.baskoparan@gmail.com"))
+                .termsOfService("Software INC.")
+                .license(new License()
+                        .name("Licence ")
+                        .url("www."))
+        );
+    }
+}
