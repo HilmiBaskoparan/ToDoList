c05fd37ddff345ec438f0a06850a9bb8003cb331
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-06-27 07:42:32 +0300

    Adding New Services to ReactJS

--- frontend/src/service/services.js
+++ frontend/src/service/services.js
@@ -1,41 +1,68 @@
 import axios from "axios";
 
+// URL
+const TODO_URL = "http://localhost:4040/api/v1/todo";
+
 // ADD
 export const addTaskService = async (newTask) => {
-  const response = await axios.post(
-    "http://localhost:4040/api/v1/todo/add",
-    newTask
-  );
+  const response = await axios.post(TODO_URL + "/add", newTask);
   return response;
 };
 
 // DELETE
 export const deleteTaskService = async (taskId) => {
-  const response = await axios.delete(
-    "http://localhost:4040/api/v1/todo/delete/" + taskId
-  );
+  const response = await axios.delete(TODO_URL + "/delete/" + taskId);
   return response;
 };
 
 // UPDATE
 export const updateTaskService = async (taskId, updateBody) => {
-  const response = await axios.put(
-    "http://localhost:4040/api/v1/todo/update/" + taskId,
-    updateBody
-  );
+  const response = await axios.put(TODO_URL + "/update/" + taskId, updateBody);
   return response;
 };
 
 // LIST
 export const getAllTasksService = async () => {
-  const response = await axios.get("http://localhost:4040/api/v1/todo/list");
+  const response = await axios.get(TODO_URL + "/list");
   return response;
 };
 
 // FIND BY ID
 export const getByIdService = async (taskId) => {
   const response = await axios.get(
-    "http://localhost:4040/api/v1/todo/find/" + taskId
+    TODO_URL + "/find/" + taskId
+  );
+  return response;
+};
+
+// DELETE ALL
+export const deleteAllService = async () => {
+  const response = await axios.delete(
+    "http://localhost:4040/api/v1/todo/delete/all"
+  );
+  return response;
+};
+
+// DELETE COMPLETED TASKS
+export const deleteCompletedTasksService = async () => {
+  const response = await axios.delete(
+    "http://localhost:4040/api/v1/todo/delete/completed"
+  );
+  return response;
+};
+
+// LIST COMPLETED TASKS
+export const getCompletedTasksService = async () => {
+  const response = await axios.get(
+    "http://localhost:4040/api/v1/todo/list/completed"
+  );
+  return response;
+};
+
+// LIST UNCOMPLETED TASKS
+export const getUncompletedTasksService = async () => {
+  const response = await axios.get(
+    "http://localhost:4040/api/v1/todo/list/incompleted"
   );
   return response;
 };
