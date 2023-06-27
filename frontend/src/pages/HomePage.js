71cc46c84c83e73eb08b1fd5076e601458445e10
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-06-27 07:23:06 +0300

    Code Line Formatting

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,245 +2,276 @@ import { useEffect, useState } from "react";
 // Popup Modal
 import Popup from "../component/Popup";
 // SERVICES
-import { 
-    getAllTasksService, 
-    addTaskService,
-    deleteTaskService,
-    updateTaskService } from "../service/services";
+import {
+  getAllTasksService,
+  addTaskService,
+  deleteTaskService,
+  updateTaskService,
+} from "../service/services";
 
 function HomePage() {
+  const [allItems, setAllItems] = useState([]);
+  const [newTaskInput, setNewTaskInput] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState({ id: 0, description: "" });
 
-    const [allItems, setAllItems] = useState([]);
-    const [newTaskInput, setNewTaskInput] = useState("");
-    const [isOpen, setIsOpen] = useState(false);
-    const [currentTodo, setCurrentTodo] = useState({ id: 0, description: "" });
+  // GET TODO LIST
+  useEffect(() => {
+    const dataList = async () => {
+      let response = await getAllTasksService();
+      setAllItems(response.data);
+    };
+    dataList();
+  }, []);
 
-    // GET TODO LIST
-    useEffect(() => {
-        const dataList = async () => {
-          let response = await getAllTasksService();
-          setAllItems(response.data);
-        };
-        dataList();
-      }, []);
+  // ADD NEW TASK
+  const addNewTask = async () => {
+    await addTaskService({ description: newTaskInput }).then((response) => {
+      const newAllItems = [...allItems];
+      newAllItems.push(response.data);
+      setAllItems(newAllItems);
+    });
+  };
 
-      // ADD NEW TASK
-      const addNewTask = async () => {
-        await addTaskService({ description: newTaskInput })
-          .then((response) => {
-            const newAllItems = [...allItems];
-            newAllItems.push(response.data);
-            setAllItems(newAllItems);
-          });
-      };
+  // DELETE TASK
+  const deleteTask = async (task) => {
+    await deleteTaskService(task?.id).then((response) => {
+      const newList = allItems.filter(
+        (task) => task?.id !== response?.data?.id
+      );
+      setAllItems(newList);
+    });
+  };
 
-      // DELETE TASK
-      const deleteTask = async (task) => {
-        await deleteTaskService(task?.id)
-          .then((response) => {
-            const newList = allItems.filter(
-              (task) => task?.id !== response?.data?.id
-            );
-            setAllItems(newList);
-          });
-      };
+  // UPDATE TASK
+  const updateDescription = async () => {
+    await updateTaskService(currentTodo?.id, {
+      description: currentTodo?.description,
+    }).then((response) => {
+      const newList = allItems?.map((variable) => {
+        if (variable?.id === response?.data?.id) {
+          return { ...variable, description: response?.data?.description };
+        }
+        return variable;
+      });
+      setAllItems(newList);
+    });
+    setIsOpen(false);
+  };
 
-      // UPDATE TASK
-      const updateDescription = async () => {
-        await updateTaskService(currentTodo?.id, {
-          description: currentTodo?.description,
-        })
-          .then((response) => {
-            const newList = allItems?.map((variable) => {
-              if (variable?.id === response?.data?.id) {
-                return { ...variable, description: response?.data?.description };
-              }
-              return variable;
-            });
-            setAllItems(newList);
-          })
-        setIsOpen(false);
-      };
+  // Open Popup
+  const openPopup = (todo) => {
+    setIsOpen(true);
+    setCurrentTodo({
+      ...currentTodo,
+      id: todo?.id,
+      description: todo?.description,
+    });
+  };
+  // Close Popup
+  const closePopup = () => {
+    setIsOpen(false);
+  };
 
-      // Open Popup
-      const openPopup = (todo) => {
-        setIsOpen(true);
-        setCurrentTodo({
-          ...currentTodo,
-          id: todo?.id,
-          description: todo?.description,
+  // Update Todo Task When Checked
+  const updateIsDone = async (task) => {
+    await updateTaskService(task?.id, { isDone: !task?.isDone })
+      .then((response) => {
+        const newList = allItems?.map((variable) => {
+          if (variable?.id === response?.data?.id) {
+            return { ...variable, isDone: response?.data?.isDone };
+          }
+          return variable;
         });
-      };
-      // Close Popup
-      const closePopup = () => {
-        setIsOpen(false);
-      };
-      
-      // Update Todo Task When Checked
-      const updateIsDone = async (task) => {
-        await updateTaskService(task?.id, { isDone: !task?.isDone })
-          .then((response) => {
-            const newList = allItems?.map((variable) => {
-              if (variable?.id === response?.data?.id) {
-                return { ...variable, isDone: response?.data?.isDone };
-              }
-              return variable;
-            });
-            setAllItems(newList);
-          })
-          .catch((err) => {
-            console.log(err.response.data.message);
-          });
-      };
-    
-    return (
-        <div className="container m-5 p-2 rounded mx-auto bg-light shadow">
+        setAllItems(newList);
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+      });
+  };
 
-            {/* TITLE */}
-            <div className="row m-1 p-4">
-                <div className="col">
-                    <div className="p-1 h1 text-center mx-auto display-inline-block">
-                        <h1>TodoInput</h1>
-                    </div>
-                </div>
-            </div>
-
-            {/* ADD NEW TASK */}
-            <div className="row m-1 p-3">
-                <div className="col col-11 mx-auto">
-                    <div className="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-center">
-                        <div className="col">
-                            <input 
-                                type="text" 
-                                className="form-control form-control-lg border-0 add-todo-input bg-transparent rounded" 
-                                placeholder="New Todo"
-                                onChange={(value) => setNewTaskInput(value.target.value)}/>
-                        </div>
-                        <div className="col-auto px-0 mx-0 mr-2">
-                            <button 
-                                type="button" 
-                                className="btn btn-primary btn-lg btn-block"
-                                onClick={addNewTask}>Add New Task</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="p-2 mx-4 border-bottom"></div>
-
-            <div className="row m-1 p-3 px-5 justify-content-end">
-                <div class="d-flex justify-content-center">
-                    <h2>TodoList</h2>
-                </div>
-                <div className="row">
-                    <button className="btn btn-primary btn-md col m-3">All</button>
-                    <button className="btn btn-primary btn-md col m-3">Completed</button>
-                    <button className="btn btn-primary btn-md col m-3">InCompleted</button>
-                </div>
-            </div>
-            
-            {/* SHOW DATA LIST */}
-            {allItems?.map((task) => (
-                <div className="row mx-1 px-5 pb-3 w-80">
-                    <div className="col mx-auto">
-                        <div className="row px-3 align-items-center todo-item rounded border">
-                            <div className="col-auto m-1 p-0 d-flex align-items-center">
-                                <h2 className="m-0 p-0">
-                                    <i className="fa fa-square-o text-primary btn m-0 p-0 d-none" data-toogle="tooltip" data-placement="bottom" title="Mark as Complete"></i>
-                                    <input 
-                                        type="checkbox" 
-                                        className="form-control-lg"
-                                        checked={task.isDone}
-                                        onChange={() => updateIsDone(task)}></input>
-                                </h2>
-                            </div>
-                            <div className="col px-1 m-1 d-flex align-items-center">
-                                <input 
-                                    style={
-                                        task?.isDone ? {textDecoration: "line-through", color: "red"} : {textDecoration: "none"}
-                                    }
-                                    type="text" 
-                                    className="form-control border-0 edit-todo-input bg-transparent rounded px-3 text-x1" 
-                                    title="Todo Item #1" 
-                                    readOnly 
-                                    disabled
-                                    value={task?.description}/>
-                            </div>
-
-                            <div className="col-auto m-1 p-0 px-3 d-none"></div>
-
-                            <div className="col-auto m-1 p-0 todo-actions">
-                                <div className="col d-flex align-items-center justify-content-end">
-                                  {/* EDIT BUTTON */}
-                                    <h5 className="m-0 p-0 px-2">
-                                        <button 
-                                            type="button" 
-                                            className="btn btn-primary"
-                                            onClick={() => openPopup(task)}>Edit</button>
-                                    </h5>
-                                    {/* DELETE BUTTON */}
-                                    <h5 className="m-0 p-0 px-2">
-                                        <button 
-                                            type="button" 
-                                            className="btn btn-danger"
-                                            onClick={() => deleteTask(task)}>Delete</button>
-                                    </h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
-            
-            <div>
-                {isOpen && (
-                    <Popup
-                    content={
-                        <>
-                            <div className="container">
-                                <b className="text-primary">UPDATE YOUR TODO</b>
-                                <div className="row px-3 align-items-center todo-item rounded border mt-4 mb-4">
-                                    <textarea
-                                        type="input"
-                                        rows="2"
-                                        className="form-control border-0 edit-todo-input bg-transparent rounded px-3 text-x1"
-                                        placeholder="Please write your todo description..."
-                                        value={currentTodo?.description}
-                                        onChange={(value) =>
-                                            setCurrentTodo({
-                                              ...currentTodo,
-                                              description: value.target.value,
-                                            })
-                                          }
-                                    />
-                                </div>
-                                <div className="col d-flex align-items-center justify-content-end">
-                                    <h5 className="m-0 p-0 px-2">
-                                        <button type="button" className="btn btn-danger" onClick={closePopup}>Close</button>
-                                    </h5>
-                                    <h5 className="m-0 p-0 px-2">
-                                        <button type="button"className="btn btn-primary" onClick={updateDescription}>Save</button>
-                                    </h5>
-                                </div>
-                            </div>
-                        </>
-                    }
-                />
-                )}
-                
-            </div>
-
-            <div className="mt-4 mb-4 col d-flex align-items-center justify-content-center">
-                <h5 className="m-0 p-0 px-2">
-                    <button type="button" className="btn btn-danger">Delete Done Tasks</button>
-                </h5>
-                <h5 className="m-0 p-0 px-2">
-                    <button type="button" className="btn btn-danger">Delete All Tasks</button>
-                </h5>
-            </div>
+  return (
+    <div className="container m-5 p-2 rounded mx-auto bg-light shadow">
+      {/* TITLE */}
+      <div className="row m-1 p-4">
+        <div className="col">
+          <div className="p-1 h1 text-center mx-auto display-inline-block">
+            <h1>TodoInput</h1>
+          </div>
         </div>
-    );
+      </div>
 
+      {/* ADD NEW TASK */}
+      <div className="row m-1 p-3">
+        <div className="col col-11 mx-auto">
+          <div className="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-center">
+            <div className="col">
+              <input
+                type="text"
+                className="form-control form-control-lg border-0 add-todo-input bg-transparent rounded"
+                placeholder="New Todo"
+                onChange={(value) => setNewTaskInput(value.target.value)}
+              />
+            </div>
+            <div className="col-auto px-0 mx-0 mr-2">
+              <button
+                type="button"
+                className="btn btn-primary btn-lg btn-block"
+                onClick={addNewTask}
+              >
+                Add New Task
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-2 mx-4 border-bottom"></div>
+
+      <div className="row m-1 p-3 px-5 justify-content-end">
+        <div class="d-flex justify-content-center">
+          <h2>TodoList</h2>
+        </div>
+        <div className="row">
+          <button className="btn btn-primary btn-md col m-3">All</button>
+          <button className="btn btn-primary btn-md col m-3">Completed</button>
+          <button className="btn btn-primary btn-md col m-3">
+            InCompleted
+          </button>
+        </div>
+      </div>
+
+      {/* SHOW DATA LIST */}
+      {allItems?.map((task) => (
+        <div className="row mx-1 px-5 pb-3 w-80">
+          <div className="col mx-auto">
+            <div className="row px-3 align-items-center todo-item rounded border">
+              <div className="col-auto m-1 p-0 d-flex align-items-center">
+                <h2 className="m-0 p-0">
+                  <i
+                    className="fa fa-square-o text-primary btn m-0 p-0 d-none"
+                    data-toogle="tooltip"
+                    data-placement="bottom"
+                    title="Mark as Complete"
+                  ></i>
+                  <input
+                    type="checkbox"
+                    className="form-control-lg"
+                    checked={task.isDone}
+                    onChange={() => updateIsDone(task)}
+                  ></input>
+                </h2>
+              </div>
+              <div className="col px-1 m-1 d-flex align-items-center">
+                <input
+                  style={
+                    task?.isDone
+                      ? { textDecoration: "line-through", color: "red" }
+                      : { textDecoration: "none" }
+                  }
+                  type="text"
+                  className="form-control border-0 edit-todo-input bg-transparent rounded px-3 text-x1"
+                  title="Todo Item #1"
+                  readOnly
+                  disabled
+                  value={task?.description}
+                />
+              </div>
+
+              <div className="col-auto m-1 p-0 px-3 d-none"></div>
+
+              <div className="col-auto m-1 p-0 todo-actions">
+                <div className="col d-flex align-items-center justify-content-end">
+                  {/* EDIT BUTTON */}
+                  <h5 className="m-0 p-0 px-2">
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      onClick={() => openPopup(task)}
+                    >
+                      Edit
+                    </button>
+                  </h5>
+                  {/* DELETE BUTTON */}
+                  <h5 className="m-0 p-0 px-2">
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={() => deleteTask(task)}
+                    >
+                      Delete
+                    </button>
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      <div>
+        {isOpen && (
+          <Popup
+            content={
+              <>
+                <div className="container">
+                  <b className="text-primary">UPDATE YOUR TODO</b>
+                  <div className="row px-3 align-items-center todo-item rounded border mt-4 mb-4">
+                    <textarea
+                      type="input"
+                      rows="2"
+                      className="form-control border-0 edit-todo-input bg-transparent rounded px-3 text-x1"
+                      placeholder="Please write your todo description..."
+                      value={currentTodo?.description}
+                      onChange={(value) =>
+                        setCurrentTodo({
+                          ...currentTodo,
+                          description: value.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                  <div className="col d-flex align-items-center justify-content-end">
+                    <h5 className="m-0 p-0 px-2">
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={closePopup}
+                      >
+                        Close
+                      </button>
+                    </h5>
+                    <h5 className="m-0 p-0 px-2">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={updateDescription}
+                      >
+                        Save
+                      </button>
+                    </h5>
+                  </div>
+                </div>
+              </>
+            }
+          />
+        )}
+      </div>
+
+      <div className="mt-4 mb-4 col d-flex align-items-center justify-content-center">
+        <h5 className="m-0 p-0 px-2">
+          <button type="button" className="btn btn-danger">
+            Delete Done Tasks
+          </button>
+        </h5>
+        <h5 className="m-0 p-0 px-2">
+          <button type="button" className="btn btn-danger">
+            Delete All Tasks
+          </button>
+        </h5>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
--- frontend/src/service/services.js
+++ frontend/src/service/services.js
@@ -2,30 +2,40 @@ import axios from "axios";
 
 // ADD
 export const addTaskService = async (newTask) => {
-  const response = await axios.post("http://localhost:4040/api/v1/todo/add", newTask);
+  const response = await axios.post(
+    "http://localhost:4040/api/v1/todo/add",
+    newTask
+  );
   return response;
 };
 
 // DELETE
 export const deleteTaskService = async (taskId) => {
-  const response = await axios.delete("http://localhost:4040/api/v1/todo/delete/" + taskId);
+  const response = await axios.delete(
+    "http://localhost:4040/api/v1/todo/delete/" + taskId
+  );
   return response;
 };
 
 // UPDATE
 export const updateTaskService = async (taskId, updateBody) => {
-  const response = await axios.put("http://localhost:4040/api/v1/todo/update/" + taskId, updateBody);
+  const response = await axios.put(
+    "http://localhost:4040/api/v1/todo/update/" + taskId,
+    updateBody
+  );
   return response;
 };
 
 // LIST
 export const getAllTasksService = async () => {
-    const response = await axios.get("http://localhost:4040/api/v1/todo/list");
-    return response;
+  const response = await axios.get("http://localhost:4040/api/v1/todo/list");
+  return response;
 };
 
 // FIND BY ID
 export const getByIdService = async (taskId) => {
-    const response = await axios.get("http://localhost:4040/api/v1/todo/find/" + taskId);
-    return response;
+  const response = await axios.get(
+    "http://localhost:4040/api/v1/todo/find/" + taskId
+  );
+  return response;
 };
