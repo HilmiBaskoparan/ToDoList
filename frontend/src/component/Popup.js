79286a1d7d73e9dd7780f8c2b78c96813f1ab3a6
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-07-05 12:12:35 +0300

    Animated Popup for Update

--- frontend/src/component/Popup.js
+++ frontend/src/component/Popup.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+// Animated Css
+// https://animate.style/
+
 const Popup = (props) => {
   return (
     <div className="popup-box">
-      <div className="box">{props.content}</div>
+      <div className="box animate__animated animate__fadeInDown animate__faster">{props.content}</div>
     </div>
   );
 };
